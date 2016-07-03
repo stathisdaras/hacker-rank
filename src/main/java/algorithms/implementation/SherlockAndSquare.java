38a93f15739c856a3124103371563e327d8b0db0
Stathis Daras <stathis.sup@hotmail.com>
2016-07-03 10:41:23 +0300

    CHG: Made maven project - Attempt to make code retroactive in order to improve performance

--- src/main/java/algorithms/implementation/SherlockAndSquare.java
+++ src/main/java/algorithms/implementation/SherlockAndSquare.java
@@ -1,29 +1,47 @@
 package algorithms.implementation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SherlockAndSquare {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		for (int i = 1; i < args.length; i = i + 2) {
-			int A = i;
-			int B = i + 1;
-			int squareNumbers;
-			while (A <= B) {
-				squareNumbers = 0;
-				if (isSquareNumber(A)) {
-					squareNumbers++;
-				}
-				A++;
+		// for every row [A, B] find sum of square numbers
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// first row is the number of rows
+		br.readLine();
+		String input;
+
+		while ((input = br.readLine()) != null) {
+			String[] testCases = input.split(" ");
+
+			Double A = Double.parseDouble(testCases[0]);
+			Double B = Double.parseDouble(testCases[1]);
+
+			int difference = (int) (Math.sqrt(B) - Math.sqrt(A));
+
+			if (isSquareNumber(B)) {
+				difference++;
 			}
-			System.out.println(squareNumbers);
+
+			if (!isSquareNumber(B) && !isSquareNumber(A) && difference != 0) {
+				difference++;
+			}
+
+			System.out.println(difference);
 		}
 	}
 
-	private static boolean isSquareNumber(int number) {
+	// if the square root of a number is an integer then it is a square number
+	private static boolean isSquareNumber(Double number) {
 		double root = Math.sqrt(number);
 		return isInteger(root);
 	}
 
+	// a number is integer if it equals its rounded value
 	private static boolean isInteger(double root) {
 		return ((root == Math.floor(root)) && !Double.isInfinite(root));
 	}
